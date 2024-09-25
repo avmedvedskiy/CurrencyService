033cabbb41dc11f64a9387cc894a3af439637c1d
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-09-25 18:53:04 +0300

    reduce field name

--- Runtime/Currency.cs
+++ Runtime/Currency.cs
@@ -8,20 +8,20 @@ namespace Currency
     {
         [SerializeField] private int _id;
 
-        [SerializeField] private int _amount;
+        [SerializeField] private int _a;
 
         public int Id => _id;
 
         public int Amount
         {
-            get => _amount;
-            set => _amount = value;
+            get => _a;
+            set => _a = value;
         }
 
         public Currency(int id, int amount)
         {
             _id = id;
-            _amount = amount;
+            _a = amount;
         }
     }
 }
